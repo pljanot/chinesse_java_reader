7d7b954415ff8bb07e30dca8acd96d016da3b5a5
Patryk Janota <patryk.l.janota@intel.com>
2020-11-23 22:59:41 +0100

    Init repository

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import picocli.CommandLine;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new CheckSum()).execute(args);
+        System.exit(exitCode);
+    }
+}
+
+@CommandLine.Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Prints the checksum (MD5 by default) of a file to STDOUT.")
+class CheckSum implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private File file;
+
+    @CommandLine.Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+    private String algorithm = "MD5";
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        byte[] fileContents = Files.readAllBytes(file.toPath());
+        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+        System.out.printf("%0" + (digest.length * 2) + "x%n", new BigInteger(1, digest));
+        return 0;
+    }
+}
